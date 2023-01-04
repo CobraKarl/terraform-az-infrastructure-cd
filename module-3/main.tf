2cac6337f9c987a849c9f345f16491f641303048
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-04 13:43:35 +0100

    added module 3 vm 9

--- module-3/main.tf
+++ module-3/main.tf
@@ -36,3 +36,27 @@ resource "azurerm_network_interface" "nicforvm" {
   }
 
 }
+
+# Create Virtual Machine 
+resource "azurerm_windows_virtual_machine" "vm" {
+    name = "vm1"
+    resource_group_name = var.RGName
+    location = var.location
+    size = "Standard_B2ms"
+    admin_username = "macmike"
+    admin_password = "Password1234"
+    network_interface_ids = [
+        azurerm_network_interface.nicforvm.id
+    ]
+    os_disk {
+      caching = "ReadWrite"
+      storage_account_type = "Standard_LRS"
+    }
+    source_image_reference {
+      publisher = "MicrosoftWindowsServer"
+      offer = "WindowsServer"
+      sku = "2019-Datacenter"
+      version = "latest"
+    }
+  
+}

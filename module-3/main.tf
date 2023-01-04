41a23e9392afae47c6c7a0aa33d64b00cde044f9
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-04 14:15:39 +0100

    added module 3 vm 15

--- module-3/main.tf
+++ module-3/main.tf
@@ -7,11 +7,12 @@ terraform {
   }
 }
 module "vnet" {
-  source = "../module-1"
+  source   = "../module-1"
   location = var.location
-  RGName = var.RGName
-  
+  RGName   = var.RGName
+
 }
+
 # Create Public IP
 resource "azurerm_public_ip" "publicip" {
   name                = "pubipforvm"
@@ -21,44 +22,65 @@ resource "azurerm_public_ip" "publicip" {
 
 }
 
-
-
-
 # Create Nic
 resource "azurerm_network_interface" "nicforvm" {
   name                = "nicforvm"
   resource_group_name = var.RGName
   location            = var.location
   ip_configuration {
-    name                 = "internal"
-    subnet_id            = "${module.vnet.azurerm_subnet_id}"
-    private_ip_address = "10.0.10.5"
+    name                          = "internal"
+    subnet_id                     = module.vnet.azurerm_subnet_id
+    private_ip_address            = "10.0.10.5"
     private_ip_address_allocation = "Static"
-    public_ip_address_id = azurerm_public_ip.publicip.id
+    public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 
 }
 
 # Create Virtual Machine 
 resource "azurerm_windows_virtual_machine" "vm" {
-    name = "vm1"
-    resource_group_name = var.RGName
-    location = var.location
-    size = "Standard_B2ms"
-    admin_username = "macmike"
-    admin_password = "Password1234"
-    network_interface_ids = [
-        azurerm_network_interface.nicforvm.id
-    ]
-    os_disk {
-      caching = "ReadWrite"
-      storage_account_type = "Standard_LRS"
-    }
-    source_image_reference {
-      publisher = "MicrosoftWindowsServer"
-      offer = "WindowsServer"
-      sku = "2019-Datacenter"
-      version = "latest"
-    }
-  
+  name                = "vm1"
+  resource_group_name = var.RGName
+  location            = var.location
+  size                = "Standard_B2ms"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nicforvm.id
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+}
+
+# Create network security group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg"
+  resource_group_name = var.RGName
+  location            = var.location
+}
+
+# Create netwrok security rule
+resource "azurerm_network_security_rule" "nsgrule_rdp" {
+  name                        = "rdp"
+  resource_group_name         = var.RGName
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "TCP"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+
 }
--- module-3/variables.tf
+++ module-3/variables.tf
@@ -7,3 +7,13 @@ variable "location" {
   type = string
 
 }
+
+variable "admin_username" {
+  type = string
+
+}
+
+variable "admin_password" {
+  type = string
+
+}

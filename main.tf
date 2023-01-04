8f3e1aa51deb0b9b53a2787b89d69e02198ffae6
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-04 10:00:43 +0100

    added module-1

--- main.tf
+++ main.tf
@@ -24,23 +24,6 @@ resource "azurerm_resource_group" "rg" {
   
 }
 
-# Create VNet
-resource "azurerm_virtual_network" "vnet" {
-    name = "vnet"
-    location = var.location
-    resource_group_name = var.RGName
-    address_space = [ "10.0.0.0/16" ]
-    depends_on = [
-      azurerm_resource_group.rg
-    ]
-  
-}
-
-# Create subnet
-resource "azurerm_subnet" "subnet" {
-    name = "subnet1"
-    resource_group_name = var.RGName
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = [ "10.0.10.0/24" ]
-  
+module "vnet" {
+    source = "./module-1"
 }
---
+++ module-1/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.91.0"
+    }
+  }
+}
+
+# Create VNet
+resource "azurerm_virtual_network" "vnet" {
+    name = "vnet"
+    location = var.location
+    resource_group_name = var.RGName
+    address_space = [ "10.0.0.0/16" ]
+    depends_on = [
+      azurerm_resource_group.rg
+    ]
+  
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet" {
+    name = "subnet1"
+    resource_group_name = var.RGName
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes = [ "10.0.10.0/24" ]
+  
+}

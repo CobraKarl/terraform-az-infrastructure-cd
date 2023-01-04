71ed5970b1386aa5dbcbe8175bef6a4e8fb17211
Mikael Karlsson <mikael.karlsson@nordcloud.com>
2023-01-04 09:07:05 +0100

    added depends on

--- main.tf
+++ main.tf
@@ -30,6 +30,9 @@ resource "azurerm_virtual_network" "vnet" {
     location = var.location
     resource_group_name = var.RGName
     address_space = [ "10.0.0.0/16" ]
+    depends_on = [
+      azurerm_resource_group.rg
+    ]
   
 }
 
